796c65449ef501649dc09a26d7ef7055dde3a3a4
Oreo AGI <oreo@cashcowlabs.io>
2026-02-09 11:31:59 -0800

    fix: proper Decap CMS OAuth handshake + fetch API

--- api/auth.js
+++ api/auth.js
@@ -1,8 +1,8 @@
 module.exports = function handler(req, res) {
-  const clientId = process.env.GITHUB_OAUTH_CLIENT_ID;
-  const scope = 'repo,user';
-  const redirectUri = 'https://www.agnivamahata.com/api/callback';
-  const authUrl = 'https://github.com/login/oauth/authorize?client_id=' + clientId + '&scope=' + scope + '&redirect_uri=' + encodeURIComponent(redirectUri);
+  var clientId = process.env.GITHUB_OAUTH_CLIENT_ID;
+  var scope = 'repo,user';
+  var redirectUri = 'https://www.agnivamahata.com/api/callback';
+  var authUrl = 'https://github.com/login/oauth/authorize?client_id=' + clientId + '&scope=' + scope + '&redirect_uri=' + encodeURIComponent(redirectUri);
   res.writeHead(302, { Location: authUrl });
   res.end();
 };
